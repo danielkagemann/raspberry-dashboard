b93531758eb1b3d5a447221ed1254a01ce3843d1
danielkagemann <info@danielkagemann.name>
2022-09-25 10:43:51 +0200

    this seems to work

--- src/video-embedded/VideoEmbedded.tsx
+++ src/video-embedded/VideoEmbedded.tsx
@@ -1,7 +1,4 @@
 import {FunctionComponent} from "react";
-import { useWindowSize } from "react-use";
-import YouTube, {YouTubeProps} from 'react-youtube';
-
 
 type Props = {
     videoId: string,
@@ -9,35 +6,33 @@ type Props = {
 };
 
 export const VideoEmbedded: FunctionComponent<Props> = ({videoId, overlay}) => {
-    const {height} = useWindowSize();
-
-    const aspectRatio = 1.78;
-    const cx = height*aspectRatio;
-
-    const opts: YouTubeProps['opts'] = {
-        width: cx,
-        height,
-        playerVars: {
-            autoplay: 1,
-            controls: 0,
-            showInfo: 0,
-            mute: 1,
-            allowFullScreen: true,
-            allow: 'autoplay'
-        },
-    };
 
     const renderOverlay = () => {
         const splits = overlay.split(' ');
         return splits.map(item => (<div className="videoembedded--overlay-item">{item}</div>))
     };
 
+    const url = `https://www.youtube.com/embed/${videoId}?autoplay=1&controls=0&showinfo=0&mute=1&allowFullScreen=true&allow=autoplay`;
+
     return (
-        <>
-            <YouTube videoId={videoId}
-                     className={'videoembedded'}
-                     opts={opts}/>
-            <div className="videoembedded--overlay">{renderOverlay()}</div>
-        </>);
+        <div className="videoembedded">
+            <iframe frameBorder="0"
+                    allowFullScreen
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                    title="."
+                    width="100%"
+                    height="100%"
+                    src={url}
+            ></iframe>
+        </div>
+    );
+
+    // return (
+    //     <>
+    //         <YouTube videoId={videoId}
+    //                  className={'videoembedded'}
+    //                  opts={opts}/>
+    //         <div className="videoembedded--overlay">{renderOverlay()}</div>
+    //     </>);
 }
 
