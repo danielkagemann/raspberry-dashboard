7b50c5f47002ddb0c0abbd5a4a77e6d5d12ee3c3
danielkagemann <info@danielkagemann.name>
2022-10-10 08:09:56 +0200

    removed react youtube. does not work as expected

--- src/components/video-embedded/VideoEmbedded.tsx
+++ src/components/video-embedded/VideoEmbedded.tsx
@@ -1,6 +1,5 @@
 import React from "react";
-import {FunctionComponent, useEffect, useRef, useState} from "react";
-import YouTube, {YouTubeProps} from 'react-youtube';
+import {FunctionComponent, useEffect, useRef} from "react";
 
 export type VideoEmbeddedType = {
     videoId: string,
@@ -8,15 +7,27 @@ export type VideoEmbeddedType = {
 };
 
 export const VideoEmbedded: FunctionComponent<VideoEmbeddedType> = ({videoId, overlay}) => {
+
     const ref = useRef<any>();
+
+    const onError = () => {};
+
+    const renderOverlay = () => {
+        const splits = overlay.split(' ');
+        return splits.map(item => (<div className="videoembedded--overlay-item">{item}</div>))
+    };
+
+    const url = `https://www.youtube.com/embed/${videoId}?autoplay=1&controls=0&showinfo=0&mute=1&allowFullScreen=true&allow=autoplay&iv_load_policy=3&modestbranding=1`;
 
     useEffect(() => {
         if (ref.current) {
-            const frame = ref.current.container;
-            const parent = frame.parentElement;
+
+            const parent = ref.current.parentElement;
+
             if (parent) {
 
-                const {height} = frame.getBoundingClientRect();
+                const {height} = ref.current.getBoundingClientRect();
+
                 const parentSize = parent.getBoundingClientRect();
 
                 const aspectRatio = 1.78; // 16:9
@@ -24,39 +35,27 @@ export const VideoEmbedded: FunctionComponent<VideoEmbeddedType> = ({videoId, ov
 
                 if (parentSize.width > cx) {
                     cx = parentSize.width;
-                    frame.style.height = `${cx / aspectRatio}px`;
+                    ref.current.style.height = `${cx / aspectRatio}px`;
                 }
                 const cx2 = cx / 2;
-                frame.style.width = `${cx}px`;
-                frame.style.left = '50%';
-                frame.style.marginLeft = `-${cx2}px`;
+
+                ref.current.style.width = `${cx}px`;
+                ref.current.style.left = '50%';
+                ref.current.style.marginLeft = `-${cx2}px`;
             }
         }
     }, [ref.current]);
 
-    const opts: YouTubeProps['opts'] = {
-        height: 640,
-        width: 480,
-        playerVars: {
-            // https://developers.google.com/youtube/player_parameters
-            autoplay: 1,
-            controls: 0,
-            showInfo: 0,
-            mute: 1,
-            allowFullScreen: true
-        },
-    };
-
-    const renderOverlay = () => {
-        const splits = overlay.split(' ');
-        return splits.map(item => (<div className="videoembedded--overlay-item">{item}</div>))
-    };
-
     return (
         <div className="videoembedded">
-            <YouTube videoId={videoId}
-                     ref={ref}
-                     opts={opts}/>
+            <iframe width={'100%'}
+                    height={'100%'}
+                    ref={ref}
+                    allowFullScreen
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                    title="."
+                    src={url}
+            ></iframe>
             <div className="videoembedded--overlay">{renderOverlay()}</div>
         </div>
     );
