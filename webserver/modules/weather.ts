721c3807bd452b05911cf8e4394edea5c50b0a4f
danielkagemann <info@danielkagemann.name>
2022-10-12 13:07:23 +0200

    retry handling for weather information

--- webserver/modules/weather.ts
+++ webserver/modules/weather.ts
@@ -4,27 +4,42 @@ import axios from 'axios';
 const debug = createDebug('minidashboard:weather');
 
 interface WeatherApi {
-    handle: any,
     url: string,
-    response: any
+    response: any,
+    timeout: number,
+    retries: number
 }
 
+const ONE_HOUR = 60 * 60 * 1000;
+const ONE_MINUTE = 60 * 1000;
+const RETRIES = 3;
+
 const _data: WeatherApi = {
-    handle: 0,
     url: '',
-    response: null
+    response: null,
+    timeout: ONE_HOUR,
+    retries: RETRIES
 };
 
 const _getData = () => {
-    axios.get(_data.url, {headers: {
+    axios.get(_data.url, {
+        headers: {
             Accept: 'application/json',
-        },})
+        },
+    })
         .then(function (response: any) {
             debug('fetched weather data');
             _data.response = response;
+            _data.retries = RETRIES;
         })
         .catch(function (error: any) {
             debug('error' + JSON.stringify(error));
+            if (_data.retries > 0) {
+                _data.retries--;
+                setTimeout(_getData, ONE_MINUTE);
+            } else {
+                _data.retries = RETRIES;
+            }
         });
 };
 
@@ -34,7 +49,7 @@ const init = (lat: number, long: number, api: string) => {
     _data.response = null;
     // send initially request
     _getData();
-    _data.handle = setInterval(_getData, 60 * 60 * 1000); // every hour
+    setInterval(_getData, ONE_HOUR); // every hour
 };
 
 export const Weather = {
